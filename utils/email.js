5dc8e3ddc3be8bcab145ac0a95ee0ed0147ab97a
Cassidy Priestley <cassipriestley@gmail.com>
2026-07-23 10:58:59 -0400

    Email.js switch from gmail to resend

--- utils/email.js
+++ utils/email.js
@@ -1,32 +1,14 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-  });
-
-  const mailOptions = {
-    from: `"MySocial" <${process.env.EMAIL}>`,
+  await resend.emails.send({
+    from: "MySocial <onboarding@resend.dev>",
     to: options.email,
     subject: options.subject,
     html: options.html,
-  };
-
-  // DEBUGGING
-  await transporter.verify();
-  console.log("SMTP verified!");
-
-  // SEND OUT EMAIL
-  await transporter.sendMail(mailOptions);
+  });
 };
 
 module.exports = sendEmail;
